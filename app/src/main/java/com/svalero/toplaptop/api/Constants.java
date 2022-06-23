b0d1e4ab2f9f69dfbee0f2c7e297e6ec9e77b203
franMV4 <79657510+franMV4@users.noreply.github.com>
2022-06-23 17:15:49 +0200

    sin cambios

--- app/src/main/java/com/svalero/toplaptop/api/Constants.java
+++ app/src/main/java/com/svalero/toplaptop/api/Constants.java
@@ -2,6 +2,7 @@ package com.svalero.toplaptop.api;
 
 public class Constants {
 
-    public final static String BASE_URL = "http://192.168.1.135:8081";
+    //   public final static String BASE_URL = "http://172.22.6.3:8081";
+    public final static String BASE_URL = "http://10.0.2.2:8081";
 
 }
